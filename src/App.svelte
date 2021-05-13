2af7cd3512809a666fcd5468eb0d247d055c6078
Eamonn de Leastar <edeleastar@wit.ie>
2021-05-13 08:40:06 +0100

    port to vite

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { setContext } from "svelte";
+  import Router from "svelte-spa-router";
+  import Header from "./components/Header.svelte";
+  import Sidebar from "./components/Sidebar.svelte";
+  import Home from "./pages/Home.svelte";
+  import Navigator from "./pages/Navigator.svelte";
+  import NotFound from "./pages/NotFound.svelte";
+  import Island from "./pages/Island.svelte";
+  import { Oileain } from "./services/oileain-api";
+
+  setContext("oileain", new Oileain());
+
+  let routes = {
+    "/": Home,
+    "/navigator": Navigator,
+    "/poi/*": Island,
+    "*": NotFound,
+  };
+</script>
+
+<svelte:head>
+  <title>Oileain</title>
+</svelte:head>
+
+<Header title="Oileain" />
+<div class="uk-container">
+  <Router {routes} />
+</div>
+<Sidebar />
---
+++ src/components/IslandDescription.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { Island } from "../services/oileain-types";
+  export let island: Island;
+</script>
+
+<div class="ui segment">
+  <div class="description">
+    {@html island.description}
+  </div>
+</div>
