75aef058a028b20982b96dd253404ef646331a6d
Eamonn de Leastar <edeleastar@wit.ie>
2021-07-23 08:12:34 +0100

    tailwind/daisy based header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,49 +1,24 @@
 <script lang="ts">
-  import Icon from "svelte-awesome";
-  import { faGithub } from "@fortawesome/free-brands-svg-icons";
-  import { faMap } from "@fortawesome/free-regular-svg-icons";
-  import { faBars, faMapMarked } from "@fortawesome/free-solid-svg-icons";
+  import {Code, Map, MenuAlt2} from "svelte-hero-icons"
+  import Icon from "./Icon.svelte";
+  import Toc from "./Toc.svelte";
+
   export let title = "";
 </script>
-<div class="container mx-auto pt-6">
+
   <div class="navbar shadow-lg bg-neutral text-neutral-content rounded-box mb-4 p-4 justify-center">
-  </div>a
-</div>
-<div class="uk-flex uk-flex-center uk-flex-middle uk-text-center uk-padding-small" uk-grid>
-  <div class="uk-padding-small">
-    <a href="#coasts-menu" uk-toggle uk-tooltip="title: Clickable list all Islands by Costal Region; pos: bottom">
-      <div uk-tooltip="title: Index of all Islands; pos: bottom">
-        <Icon data={faBars} scale="2" />
-      </div>
-    </a>
-  </div>
-  <div class="uk-padding-small">
-    <a href="/#/" uk-tooltip="title: Islands of Ireland; pos:bottom">
-      <Icon data={faMap} scale="3" />
-    </a>
-  </div>
-  <div class="uk-child-width-1-2@s uk-card uk-card-default uk-grid-collapse" uk-grid>
-    <div>
-      <div class="uk-tile uk-tile-default uk-padding-small">
-        <h2>{title}</h2>
-      </div>
+   <Toc />
+    <div class="flex-1 hidden px-2 mx-2 lg:block">
+      <p class="text-lg font-bold">Oileain</p>
+      An experiment in exploring <a href="http://www.oileain.org/">David Walsh's Oileain</a>. Prepared by Eamonn de
+      Leastar (edeleastar@gmail.com)
     </div>
-    <div>
-      <div class="uk-tile uk-tile-muted uk-padding-small">
-        <div class="uk-text-small">
-          An experiment in exploring
-          <a href="http://www.oileain.org/">David Walsh's Oileain</a>. Prepared by Eamonn de Leastar (edeleastar@gmail.com)
-        </div>
-      </div>
+    <div class="flex-wrap lg:flex-none shadow-lg bg-neutral text-neutral-content rounded-box horizontal ml-2 mt-2 lg:mt-0">
+      <Icon type={Map} toolTip="Explore Island Maps" link="/#/navigator" button="true"/>
+      <Icon type={Code} toolTip="Application Source" link="https://github.com/edeleastar/oileain-vite" button="true"/>
+      <a href="http://www.oileain.org/" data-tip="Oileian Book Site" class="tooltip capitalize"> <img
+          data-src="http://www.oileain.org/wp-content/uploads/2015/05/oileain_2X.png" width="70px" height="" alt=""
+          uk-img/> </a>
     </div>
   </div>
-  <a href="/#/navigator" uk-tooltip="title: Explore Island Maps; pos:bottom">
-    <Icon data={faMapMarked} scale="3" />
-  </a>
-  <a href="http://www.oileain.org/" uk-tooltip="title: Oileain Book Site; pos:bottom">
-    <img data-src="http://www.oileain.org/wp-content/uploads/2015/05/oileain_2X.png" width="70px" height="" alt="" uk-img />
-  </a>
-  <a href="https://github.com/wit-hdip-comp-sci-2020/oileain-svelte" uk-tooltip="title: Project Source; pos:bottom">
-    <Icon data={faGithub} scale="3" />
-  </a>
-</div>
+
