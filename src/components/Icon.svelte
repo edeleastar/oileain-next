ae1a3bcf2fe8943cf1b2085c30dc4dc91efe064e
Eamonn de Leastar <edeleastar@wit.ie>
2021-07-23 08:12:14 +0100

    icon + sidebar components

---
+++ src/components/Icon.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Icon from "svelte-hero-icons";
+
+  export let type;
+  export let toolTip = "";
+  export let link = "";
+  export let target = "";
+  export let button = "";
+  export let colour = "";
+</script>
+
+
+<div data-tip="{toolTip}" class="tooltip capitalize">
+  {#if link}
+    {#if button}
+      <button tabindex="0" class="btn btn-square btn-ghost">
+        <a target="{target}" href="{link}">
+          <Icon src={type} class="w-5 h-5 text-{colour}"/>
+        </a>
+      </button>
+    {:else}
+      <a target="{target}" href="{link}">
+        <Icon src={type} class="w-5 h-5 text-{colour}"/>
+      </a>
+    {/if}
+  {:else}
+    {#if button}
+      <button tabindex="0" class="btn btn-square btn-ghost">
+        <Icon src={type} class="w-5 h-5 text-{colour}"/>
+      </button>
+    {:else}
+      <Icon src={type} class="w-5 h-5 text-{colour}"/>
+    {/if}
+  {/if}
+</div>
+
---
+++ src/components/Toc.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Icon from "./Icon.svelte";
+  import {MenuAlt2} from "svelte-hero-icons"
+</script>
+
+<label for="my-drawer" class="btn btn-primary drawer-button">
+  <Icon type={MenuAlt2} toolTip="Discover Islands"></Icon>
+</label>
