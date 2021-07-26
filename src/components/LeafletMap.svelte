1c216a46d6f6aed3c0c2ed006e4d3d2405980ef1
Eamonn de Leastar <edeleastar@wit.ie>
2021-07-26 11:29:08 +0100

    experiment with icon

--- src/components/LeafletMap.svelte
+++ src/components/LeafletMap.svelte
@@ -20,6 +20,18 @@
   let control: Control.Layers;
   let overlays: Control.LayersObject = {};
 
+  var greenIcon = L.icon({
+    iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
+    shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
+
+
   let baseLayers = {
     Terrain: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 17,
@@ -70,7 +82,7 @@
         draggable: false,
         spec: markerSpec
       }
-      let marker = L.marker([markerSpec.location.lat, markerSpec.location.lng],  markerOptions);
+      let marker = L.marker([markerSpec.location.lat, markerSpec.location.lng],  {icon: greenIcon, options:markerOptions});
       marker.bindTooltip(markerSpec.title);
       marker.addTo(group);
       marker.addTo(group).on("click", (event: any) => {
