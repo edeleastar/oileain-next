3ef3e9470a0beac13233889a5644bcd78226b1d0
Eamonn de Leastar <edeleastar@wit.ie>
2021-07-26 11:41:43 +0100

    revised custom icon

--- src/components/LeafletMap.svelte
+++ src/components/LeafletMap.svelte
@@ -21,16 +21,18 @@
   let overlays: Control.LayersObject = {};
 
   var greenIcon = L.icon({
-    iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
-    shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png',
-
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+   // iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
+    iconUrl: '/images/marker-icon-2x.png',
+    iconSize: [25, 40],
+    iconAnchor: [15.5, 40], // point of the icon which will correspond to marker's location
   });
 
+  // const markerIcon = L.icon({
+  //   iconUrl: `https://api.geoapify.com/v1/icon?size=xx-large&type=awesome&color=%233e9cfe&icon=paw&apiKey=${myAPIKey}`,
+  //   iconSize: [31, 46], // size of the icon
+  //   iconAnchor: [15.5, 42], // point of the icon which will correspond to marker's location
+  //   popupAnchor: [0, -45] // point from which the popup should open relative to the iconAnchor
+  // });
 
   let baseLayers = {
     Terrain: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
