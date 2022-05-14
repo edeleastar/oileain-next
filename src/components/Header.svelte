bad28435d48b417b843ee20f3cf4078d093ce28a
Eamonn de Leastar <edeleastar@wit.ie>
2022-05-14 09:34:18 +0100

    header adhustments

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,12 +7,17 @@
 </script>
 
 <div class="navbar shadow-lg bg-neutral text-neutral-content rounded-box mb-4 p-4 justify-center">
-  <a href="http://www.oileain.org/" data-tip="Oileian Book Site" target="_blank" class="tooltip capitalize"> <img
-      src="http://www.oileain.org/wp-content/uploads/2015/05/oileain_2X.png" alt="oileain" width="70px"> </a>
+
+  <a href="https://www.bookdepository.com/Oileain---the-Irish-Islands-Guide-David-Walsh/9781906095376?redirected=true&utm_medium=Google&utm_campaign=Base1&utm_source=IE&utm_content=Oileain---the-Irish-Islands-Guide&selectCurrency=EUR&w=AFFPAU96ZR10PUA8VTCJ&gclid=Cj0KCQjwpv2TBhDoARIsALBnVnln5gXm8nVuixUGhOMnZ_wixd4kDkp465rG6Vb4bOOcyGeHaQ2RkG4aAo0wEALw_wcB"
+     target="_blank" data-tip="Oileáin – by David Walsh" class="tooltip tooltip-right capitalize"> <img
+      src="/favicon.png" width="60px" alt="Oileáin – by David Walsh"> </a>
   <div class="flex-1 hidden px-2 mx-2 lg:block">
-    <p class="text-lg font-bold">Oileain</p>
+    <p class="text-base font-thin">Oileáin – The guide to the Irish islands by David Walsh, site by Eamonn de
+      Leastar</p>
     {#if $currentPoi !== null}
-      {@html $currentPoi.title} [{$currentPoi.lat},{$currentPoi.lng}]
+      <div class="text-sm font-thin">
+        {@html $currentPoi.title} [{$currentPoi.lat},{$currentPoi.lng}]
+      </div>
     {/if}
   </div>
   <div
